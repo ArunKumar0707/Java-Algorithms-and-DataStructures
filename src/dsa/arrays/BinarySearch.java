19c77a741ffa215f89646956f1ed044ace721bc0
Arun <aka1642001@gmail.com>
2025-11-28 15:28:46 +0530

    Added algorithm for Binary Search & Order Agnostic Binary Search .

---
+++ src/dsa/arrays/BinarySearch.java
@@ -0,0 +1,25 @@
+package dsa.arrays;
+
+public class BinarySearch {
+    static void main(String[] args) {
+        int arr[] = {1, 22, 44, 55, 66, 77, 88, 99};
+        int target = 88;
+        System.out.println("index : " + binarySearch(arr, target));
+    }
+
+    private static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        for (int i = start; i <= end; i++) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/dsa/arrays/OrderAgnosticBinarySearch.java
@@ -0,0 +1,33 @@
+package dsa.arrays;
+
+public class OrderAgnosticBinarySearch {
+    static void main(String[] args) {
+        int arr[] = {99, 87, 76, 54, 54, 45, 23, 21, 10, 5, 3, 0, -22, -100};
+        int target = 23;
+        System.out.println("index : " + orderAgnosticBinarySearch(arr, target));
+    }
+
+    private static int orderAgnosticBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
+        for (int i = start; i <= end; i++) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) return mid;
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

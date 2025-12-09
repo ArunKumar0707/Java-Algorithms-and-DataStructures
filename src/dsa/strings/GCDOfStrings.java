93836452abf936c31d42d5e99d00be02bec6b5d4
Arun <aka1642001@gmail.com>
2025-12-09 21:47:45 +0530

    Added algorithm to find the GCD of two strings.

---
+++ src/dsa/strings/GCDOfStrings.java
@@ -0,0 +1,24 @@
+package dsa.strings;
+
+public class GCDOfStrings {
+    static void main(String[] args) {
+        System.out.println(gcdOfStrings("ABABAB", "ABAB"));
+    }
+
+    public static String gcdOfStrings(String str1, String str2) {
+        //checking non-zero
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+
+    public static int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }
+    }
+}

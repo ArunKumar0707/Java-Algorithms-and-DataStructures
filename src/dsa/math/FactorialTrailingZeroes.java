5b00ad684b230993fc40b10ae88b5398bc0513d6
Arun <aka1642001@gmail.com>
2026-01-25 23:07:39 +0530

    Added algorithm to count trailing zeroes in factorial.

---
+++ src/dsa/math/FactorialTrailingZeroes.java
@@ -0,0 +1,21 @@
+package dsa.math;
+
+public class FactorialTrailingZeroes {
+
+    public static int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            n = n / 5;
+            count += n;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(trailingZeroes(5));
+        System.out.println(trailingZeroes(125));
+    }
+}
+

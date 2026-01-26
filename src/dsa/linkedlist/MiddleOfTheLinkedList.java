1144274e47933eab69cce38894ac579125c93755
Arun <aka1642001@gmail.com>
2026-01-26 23:48:33 +0530

    Added algorithm to find the middle of a singly linked list.

---
+++ src/dsa/linkedlist/MiddleOfTheLinkedList.java
@@ -0,0 +1,41 @@
+package dsa.linkedlist;
+
+public class MiddleOfTheLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head =
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(3,
+                                        new ListNode(4,
+                                                new ListNode(5)))));
+
+        ListNode res = middleNode(head);
+        System.out.println(res.val);
+    }
+}
+

b2ff5308267645967a3e99af00e6281ae210ecbc
Arun <aka1642001@gmail.com>
2026-01-27 23:55:22 +0530

    Added algorithm to find the middle of a singly linked list.

--- src/dsa/math/ArmstrongNumber.java
+++ src/dsa/math/ArmstrongNumber.java
@@ -7,7 +7,7 @@ public class ArmstrongNumber {
     static void main() {
         try {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter any number : ");
+            System.out.println("Enter any number :  ");
             int n = sc.nextInt();
             System.out.println(isArmstrongNumber(n));
         } catch (InputMismatchException e) {

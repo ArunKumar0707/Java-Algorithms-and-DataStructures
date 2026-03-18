187bacfb674a76f8897838b72a7b7f936a1589d9
Arun <aka1642001@gmail.com>
2026-03-18 15:38:34 +0530

    Added Algorithm to reverse a string in-place using two-pointer approach.

---
+++ src/dsa/strings/ReverseString.java
@@ -0,0 +1,27 @@
+package dsa.strings;
+
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+        int n = s.length;
+        boolean isEven = (n % 2 == 0);
+
+        for (int i = 0; i < n / 2; i++) {
+            if (!isEven && i == n / 2) break;
+
+            char temp = s[i];
+            s[i] = s[n - 1 - i];
+            s[n - 1 - i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] test1 = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(test1);
+        System.out.println(test1);
+
+        char[] test2 = {'H', 'a', 'n', 'n', 'a', 'h'};
+        reverseString(test2);
+        System.out.println(test2);
+    }
+}

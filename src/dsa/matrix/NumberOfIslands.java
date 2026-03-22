e1d05c2b152ef872edba91b788bfa05917199179
Arun <aka1642001@gmail.com>
2026-03-22 15:45:24 +0530

    Added solution to Number of Islands

---
+++ src/dsa/matrix/NumberOfIslands.java
@@ -0,0 +1,55 @@
+package dsa.matrix;
+
+public class NumberOfIslands {
+
+    public static int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int c = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+    private static void dfs(char[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid1 = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+
+        char[][] grid2 = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+
+        System.out.println(numIslands(grid1)); // 1
+        System.out.println(numIslands(grid2)); // 3
+    }
+}
